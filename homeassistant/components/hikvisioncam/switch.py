4401de3f130e0b3cd8baf36c43eb4ae034ba9f61
Robert Hillis <tkdrob4390@yahoo.com>
2021-07-15 16:57:29 -0400

    Use entity class attributes for hikvisioncam

--- homeassistant/components/hikvisioncam/switch.py
+++ homeassistant/components/hikvisioncam/switch.py
@@ -64,38 +64,27 @@ class HikvisionMotionSwitch(SwitchEntity):
 
     def __init__(self, name, hikvision_cam):
         """Initialize the switch."""
-        self._name = name
+        self._attr_name = name
         self._hikvision_cam = hikvision_cam
-        self._state = STATE_OFF
-
-    @property
-    def name(self):
-        """Return the name of the device if any."""
-        return self._name
-
-    @property
-    def state(self):
-        """Return the state of the device if any."""
-        return self._state
-
-    @property
-    def is_on(self):
-        """Return true if device is on."""
-        return self._state == STATE_ON
+        self._attr_state = STATE_OFF
 
     def turn_on(self, **kwargs):
         """Turn the device on."""
         _LOGGING.info("Turning on Motion Detection ")
+        self._attr_state = STATE_ON
+        self._attr_is_on = True
         self._hikvision_cam.enable_motion_detection()
 
     def turn_off(self, **kwargs):
         """Turn the device off."""
         _LOGGING.info("Turning off Motion Detection ")
+        self._attr_state = STATE_OFF
+        self._attr_is_on = False
         self._hikvision_cam.disable_motion_detection()
 
     def update(self):
         """Update Motion Detection state."""
         enabled = self._hikvision_cam.is_motion_detection_enabled()
         _LOGGING.info("enabled: %s", enabled)
-
-        self._state = STATE_ON if enabled else STATE_OFF
+        self._attr_state = STATE_ON if enabled else STATE_OFF
+        self._attr_is_on = self.state == STATE_ON
