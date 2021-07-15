403dc0e260c120f101a8ae083447f0fa46812c70
Robert Hillis <tkdrob4390@yahoo.com>
2021-07-15 15:11:23 -0400

    Use entity class attributes for heatmiser

--- homeassistant/components/heatmiser/climate.py
+++ homeassistant/components/heatmiser/climate.py
@@ -68,80 +68,38 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
 class HeatmiserV3Thermostat(ClimateEntity):
     """Representation of a HeatmiserV3 thermostat."""
 
+    _attr_hvac_modes = [HVAC_MODE_HEAT, HVAC_MODE_OFF]
+    _attr_supported_features = SUPPORT_TARGET_TEMPERATURE
+
     def __init__(self, therm, device, uh1):
         """Initialize the thermostat."""
         self.therm = therm(device[CONF_ID], "prt", uh1)
         self.uh1 = uh1
-        self._name = device[CONF_NAME]
-        self._current_temperature = None
-        self._target_temperature = None
-        self._id = device
+        self._attr_name = device[CONF_NAME]
         self.dcb = None
-        self._hvac_mode = HVAC_MODE_HEAT
-        self._temperature_unit = None
-
-    @property
-    def supported_features(self):
-        """Return the list of supported features."""
-        return SUPPORT_TARGET_TEMPERATURE
-
-    @property
-    def name(self):
-        """Return the name of the thermostat, if any."""
-        return self._name
-
-    @property
-    def temperature_unit(self):
-        """Return the unit of measurement which this thermostat uses."""
-        return self._temperature_unit
-
-    @property
-    def hvac_mode(self) -> str:
-        """Return hvac operation ie. heat, cool mode.
-
-        Need to be one of HVAC_MODE_*.
-        """
-        return self._hvac_mode
-
-    @property
-    def hvac_modes(self) -> list[str]:
-        """Return the list of available hvac operation modes.
-
-        Need to be a subset of HVAC_MODES.
-        """
-        return [HVAC_MODE_HEAT, HVAC_MODE_OFF]
-
-    @property
-    def current_temperature(self):
-        """Return the current temperature."""
-        return self._current_temperature
-
-    @property
-    def target_temperature(self):
-        """Return the temperature we try to reach."""
-        return self._target_temperature
+        self._attr_hvac_mode = HVAC_MODE_HEAT
 
     def set_temperature(self, **kwargs):
         """Set new target temperature."""
         temperature = kwargs.get(ATTR_TEMPERATURE)
-        self._target_temperature = int(temperature)
-        self.therm.set_target_temp(self._target_temperature)
+        self._attr_target_temperature = int(temperature)
+        self.therm.set_target_temp(self.target_temperature)
 
     def update(self):
         """Get the latest data."""
         self.uh1.reopen()
         if not self.uh1.status:
-            _LOGGER.error("Failed to update device %s", self._name)
+            _LOGGER.error("Failed to update device %s", self.name)
             return
         self.dcb = self.therm.read_dcb()
-        self._temperature_unit = (
+        self._attr_temperature_unit = (
             TEMP_CELSIUS
             if (self.therm.get_temperature_format() == "C")
             else TEMP_FAHRENHEIT
         )
-        self._current_temperature = int(self.therm.get_floor_temp())
-        self._target_temperature = int(self.therm.get_target_temp())
-        self._hvac_mode = (
+        self._attr_current_temperature = int(self.therm.get_floor_temp())
+        self._attr_target_temperature = int(self.therm.get_target_temp())
+        self._attr_hvac_mode = (
             HVAC_MODE_OFF
             if (int(self.therm.get_current_state()) == 0)
             else HVAC_MODE_HEAT
