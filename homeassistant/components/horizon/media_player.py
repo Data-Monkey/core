3149c500c815704a352c8bed070a1899f0ce2dc0
Robert Hillis <tkdrob4390@yahoo.com>
2021-07-16 08:32:33 -0400

    Use entity class attributes for horizon

--- homeassistant/components/horizon/media_player.py
+++ homeassistant/components/horizon/media_player.py
@@ -81,83 +81,69 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
 class HorizonDevice(MediaPlayerEntity):
     """Representation of a Horizon HD Recorder."""
 
+    _attr_supported_features = SUPPORT_HORIZON
+
     def __init__(self, client, name, remote_keys):
         """Initialize the remote."""
         self._client = client
-        self._name = name
-        self._state = None
+        self._attr_name = name
         self._keys = remote_keys
-
-    @property
-    def name(self):
-        """Return the name of the remote."""
-        return self._name
-
-    @property
-    def state(self):
-        """Return the state of the device."""
-        return self._state
-
-    @property
-    def supported_features(self):
-        """Flag media player features that are supported."""
-        return SUPPORT_HORIZON
 
     @util.Throttle(MIN_TIME_BETWEEN_SCANS, MIN_TIME_BETWEEN_FORCED_SCANS)
     def update(self):
         """Update State using the media server running on the Horizon."""
         try:
             if self._client.is_powered_on():
-                self._state = STATE_PLAYING
+                self._attr_state = STATE_PLAYING
             else:
-                self._state = STATE_OFF
+                self._attr_state = STATE_OFF
         except OSError:
-            self._state = STATE_OFF
+            self._attr_state = STATE_OFF
 
     def turn_on(self):
         """Turn the device on."""
-        if self._state == STATE_OFF:
+        if self.state == STATE_OFF:
             self._send_key(self._keys.POWER)
 
     def turn_off(self):
         """Turn the device off."""
-        if self._state != STATE_OFF:
+        if self.state != STATE_OFF:
             self._send_key(self._keys.POWER)
 
     def media_previous_track(self):
         """Channel down."""
         self._send_key(self._keys.CHAN_DOWN)
-        self._state = STATE_PLAYING
+        self._attr_state = STATE_PLAYING
 
     def media_next_track(self):
         """Channel up."""
         self._send_key(self._keys.CHAN_UP)
-        self._state = STATE_PLAYING
+        self._attr_state = STATE_PLAYING
 
     def media_play(self):
         """Send play command."""
         self._send_key(self._keys.PAUSE)
-        self._state = STATE_PLAYING
+        self._attr_state = STATE_PLAYING
 
     def media_pause(self):
         """Send pause command."""
         self._send_key(self._keys.PAUSE)
-        self._state = STATE_PAUSED
+        self._attr_state = STATE_PAUSED
 
     def media_play_pause(self):
         """Send play/pause command."""
         self._send_key(self._keys.PAUSE)
-        if self._state == STATE_PAUSED:
-            self._state = STATE_PLAYING
+        if self.state == STATE_PAUSED:
+            self._attr_state = STATE_PLAYING
         else:
-            self._state = STATE_PAUSED
+            self._attr_state = STATE_PAUSED
 
     def play_media(self, media_type, media_id, **kwargs):
         """Play media / switch to channel."""
         if MEDIA_TYPE_CHANNEL == media_type:
             try:
                 self._select_channel(int(media_id))
-                self._state = STATE_PLAYING
+                self._attr_state = STATE_PLAYING
             except ValueError:
                 _LOGGER.error("Invalid channel: %s", media_id)
         else:
@@ -184,7 +170,7 @@ class HorizonDevice(MediaPlayerEntity):
             elif channel:
                 self._client.select_channel(channel)
         except OSError as msg:
-            _LOGGER.error("%s disconnected: %s. Trying to reconnect", self._name, msg)
+            _LOGGER.error("%s disconnected: %s. Trying to reconnect", self.name, msg)
 
             # for reconnect, first gracefully disconnect
             self._client.disconnect()
@@ -193,11 +179,11 @@ class HorizonDevice(MediaPlayerEntity):
                 self._client.connect()
                 self._client.authorize()
             except AuthenticationError as msg:
-                _LOGGER.error("Authentication to %s failed: %s", self._name, msg)
+                _LOGGER.error("Authentication to %s failed: %s", self.name, msg)
                 return
             except OSError as msg:
                 # occurs if horizon box is offline
-                _LOGGER.error("Reconnect to %s failed: %s", self._name, msg)
+                _LOGGER.error("Reconnect to %s failed: %s", self.name, msg)
                 return
 
             self._send(key=key, channel=channel)
