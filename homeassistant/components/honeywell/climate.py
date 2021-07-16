e3151b8cb87719f7faedca69adf3e7161ab9c3f9
Robert Hillis <tkdrob4390@yahoo.com>
2021-07-16 08:29:47 -0400

    Use entity class attributes for honeywell

--- homeassistant/components/honeywell/climate.py
+++ homeassistant/components/honeywell/climate.py
@@ -3,7 +3,6 @@ from __future__ import annotations
 
 import datetime
 import logging
-from typing import Any
 
 import requests
 import somecomfort
@@ -154,15 +153,15 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
 class HoneywellUSThermostat(ClimateEntity):
     """Representation of a Honeywell US Thermostat."""
 
+    _attr_preset_modes = [PRESET_NONE, PRESET_AWAY]
+
     def __init__(
         self, client, device, cool_away_temp, heat_away_temp, username, password
     ):
         """Initialize the thermostat."""
         self._client = client
         self._device = device
-        self._cool_away_temp = cool_away_temp
-        self._heat_away_temp = heat_away_temp
-        self._away = False
+        self._attr_name = device.name
         self._username = username
         self._password = password
 
@@ -171,145 +170,25 @@ class HoneywellUSThermostat(ClimateEntity):
         # not all honeywell HVACs support all modes
         mappings = [v for k, v in HVAC_MODE_TO_HW_MODE.items() if device.raw_ui_data[k]]
         self._hvac_mode_map = {k: v for d in mappings for k, v in d.items()}
-
-        self._supported_features = (
+        self._attr_hvac_modes = list(self._hvac_mode_map)
+        self._attr_temperature_unit = (
+            TEMP_CELSIUS if device.temperature_unit == "C" else TEMP_FAHRENHEIT
+        )
+        self._attr_supported_features = (
             SUPPORT_PRESET_MODE
             | SUPPORT_TARGET_TEMPERATURE
             | SUPPORT_TARGET_TEMPERATURE_RANGE
         )
-
         if device._data["canControlHumidification"]:
-            self._supported_features |= SUPPORT_TARGET_HUMIDITY
-
+            self._attr_supported_features |= SUPPORT_TARGET_HUMIDITY
         if device.raw_ui_data["SwitchEmergencyHeatAllowed"]:
-            self._supported_features |= SUPPORT_AUX_HEAT
-
-        if not device._data["hasFan"]:
-            return
-
-        # not all honeywell fans support all modes
-        mappings = [v for k, v in FAN_MODE_TO_HW.items() if device.raw_fan_data[k]]
-        self._fan_mode_map = {k: v for d in mappings for k, v in d.items()}
-
-        self._supported_features |= SUPPORT_FAN_MODE
-
-    @property
-    def name(self) -> str | None:
-        """Return the name of the honeywell, if any."""
-        return self._device.name
-
-    @property
-    def extra_state_attributes(self) -> dict[str, Any]:
-        """Return the device specific state attributes."""
-        data = {}
-        data[ATTR_FAN_ACTION] = "running" if self._device.fan_running else "idle"
-        data[ATTR_PERMANENT_HOLD] = self._is_permanent_hold()
-        if self._device.raw_dr_data:
-            data["dr_phase"] = self._device.raw_dr_data.get("Phase")
-        return data
-
-    @property
-    def supported_features(self) -> int:
-        """Return the list of supported features."""
-        return self._supported_features
-
-    @property
-    def min_temp(self) -> float:
-        """Return the minimum temperature."""
-        if self.hvac_mode in [HVAC_MODE_COOL, HVAC_MODE_HEAT_COOL]:
-            return self._device.raw_ui_data["CoolLowerSetptLimit"]
-        if self.hvac_mode == HVAC_MODE_HEAT:
-            return self._device.raw_ui_data["HeatLowerSetptLimit"]
-        return None
-
-    @property
-    def max_temp(self) -> float:
-        """Return the maximum temperature."""
-        if self.hvac_mode == HVAC_MODE_COOL:
-            return self._device.raw_ui_data["CoolUpperSetptLimit"]
-        if self.hvac_mode in [HVAC_MODE_HEAT, HVAC_MODE_HEAT_COOL]:
-            return self._device.raw_ui_data["HeatUpperSetptLimit"]
-        return None
-
-    @property
-    def temperature_unit(self) -> str:
-        """Return the unit of measurement."""
-        return TEMP_CELSIUS if self._device.temperature_unit == "C" else TEMP_FAHRENHEIT
-
-    @property
-    def current_humidity(self) -> int | None:
-        """Return the current humidity."""
-        return self._device.current_humidity
-
-    @property
-    def hvac_mode(self) -> str:
-        """Return hvac operation ie. heat, cool mode."""
-        return HW_MODE_TO_HVAC_MODE[self._device.system_mode]
-
-    @property
-    def hvac_modes(self) -> list[str]:
-        """Return the list of available hvac operation modes."""
-        return list(self._hvac_mode_map)
-
-    @property
-    def hvac_action(self) -> str | None:
-        """Return the current running hvac operation if supported."""
-        if self.hvac_mode == HVAC_MODE_OFF:
-            return None
-        return HW_MODE_TO_HA_HVAC_ACTION[self._device.equipment_output_status]
-
-    @property
-    def current_temperature(self) -> float | None:
-        """Return the current temperature."""
-        return self._device.current_temperature
-
-    @property
-    def target_temperature(self) -> float | None:
-        """Return the temperature we try to reach."""
-        if self.hvac_mode == HVAC_MODE_COOL:
-            return self._device.setpoint_cool
-        if self.hvac_mode == HVAC_MODE_HEAT:
-            return self._device.setpoint_heat
-        return None
-
-    @property
-    def target_temperature_high(self) -> float | None:
-        """Return the highbound target temperature we try to reach."""
-        if self.hvac_mode == HVAC_MODE_HEAT_COOL:
-            return self._device.setpoint_cool
-        return None
-
-    @property
-    def target_temperature_low(self) -> float | None:
-        """Return the lowbound target temperature we try to reach."""
-        if self.hvac_mode == HVAC_MODE_HEAT_COOL:
-            return self._device.setpoint_heat
-        return None
-
-    @property
-    def preset_mode(self) -> str | None:
-        """Return the current preset mode, e.g., home, away, temp."""
-        return PRESET_AWAY if self._away else None
-
-    @property
-    def preset_modes(self) -> list[str] | None:
-        """Return a list of available preset modes."""
-        return [PRESET_NONE, PRESET_AWAY]
-
-    @property
-    def is_aux_heat(self) -> str | None:
-        """Return true if aux heater."""
-        return self._device.system_mode == "emheat"
-
-    @property
-    def fan_mode(self) -> str | None:
-        """Return the fan setting."""
-        return HW_FAN_MODE_TO_HA[self._device.fan_mode]
-
-    @property
-    def fan_modes(self) -> list[str] | None:
-        """Return the list of available fan modes."""
-        return list(self._fan_mode_map)
+            self._attr_supported_features |= SUPPORT_AUX_HEAT
+        if device._data["hasFan"]:
+            # not all honeywell fans support all modes
+            mappings = [v for k, v in FAN_MODE_TO_HW.items() if device.raw_fan_data[k]]
+            self._fan_mode_map = {k: v for d in mappings for k, v in d.items()}
+            self._attr_fan_modes = list(self._fan_mode_map)
+            self._attr_supported_features |= SUPPORT_FAN_MODE
 
     def _is_permanent_hold(self) -> bool:
         heat_status = self._device.raw_ui_data.get("StatusHeat", 0)
@@ -370,7 +249,7 @@ class HoneywellUSThermostat(ClimateEntity):
         work the way it should. For example: If you set a temperature manually
         it doesn't get overwritten when away mode is switched on.
         """
-        self._away = True
+        self._attr_preset_mode = PRESET_AWAY
         try:
             # Get current mode
             mode = self._device.system_mode
@@ -392,7 +271,7 @@ class HoneywellUSThermostat(ClimateEntity):
 
     def _turn_away_mode_off(self) -> None:
         """Turn away off."""
-        self._away = False
+        self._attr_preset_mode = None
         try:
             # Disabling all hold modes
             self._device.hold_cool = False
@@ -469,3 +348,43 @@ class HoneywellUSThermostat(ClimateEntity):
         _LOGGER.debug(
             "latestData = %s ", self._device._data  # pylint: disable=protected-access
         )
+
+        data = {}
+        data[ATTR_FAN_ACTION] = "running" if self._device.fan_running else "idle"
+        data[ATTR_PERMANENT_HOLD] = self._is_permanent_hold()
+        if self._device.raw_dr_data:
+            data["dr_phase"] = self._device.raw_dr_data.get("Phase")
+        self._attr_extra_state_attributes = data
+
+        self._attr_min_temp = self._attr_max_temp = None
+        if self.hvac_mode in [HVAC_MODE_COOL, HVAC_MODE_HEAT_COOL]:
+            self._attr_min_temp = self._device.raw_ui_data["CoolLowerSetptLimit"]
+        elif self.hvac_mode == HVAC_MODE_HEAT:
+            self._attr_min_temp = self._device.raw_ui_data["HeatLowerSetptLimit"]
+        if self.hvac_mode == HVAC_MODE_COOL:
+            self._attr_max_temp = self._device.raw_ui_data["CoolUpperSetptLimit"]
+        elif self.hvac_mode in [HVAC_MODE_HEAT, HVAC_MODE_HEAT_COOL]:
+            self._attr_max_temp = self._device.raw_ui_data["HeatUpperSetptLimit"]
+
+        self._attr_current_humidity = self._device.current_humidity
+        self._attr_hvac_mode = HW_MODE_TO_HVAC_MODE[self._device.system_mode]
+        self._attr_hvac_action = None
+        if self.hvac_mode != HVAC_MODE_OFF:
+            self._attr_hvac_action = HW_MODE_TO_HA_HVAC_ACTION[
+                self._device.equipment_output_status
+            ]
+
+        self._attr_target_temperature = None
+        self._attr_target_temperature_high = None
+        self._attr_target_temperature_low = None
+        self._attr_current_temperature = self._device.current_temperature
+        if self.hvac_mode == HVAC_MODE_COOL:
+            self._attr_target_temperature = self._device.setpoint_cool
+        elif self.hvac_mode == HVAC_MODE_HEAT:
+            self._attr_target_temperature = self._device.setpoint_heat
+        if self.hvac_mode == HVAC_MODE_HEAT_COOL:
+            self._attr_target_temperature_high = self._device.setpoint_cool
+            self._attr_target_temperature_low = self._device.setpoint_heat
+
+        self._attr_is_aux_heat = self._device.system_mode == "emheat"
+        self._attr_fan_mode = HW_FAN_MODE_TO_HA[self._device.fan_mode]
