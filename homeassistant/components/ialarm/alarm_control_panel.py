774fded8cd532df866c1d14827ef5aa85c39756e
Robert Hillis <tkdrob4390@yahoo.com>
2021-07-17 17:57:14 -0400

    Use entity class attributes for ialarm

--- homeassistant/components/ialarm/alarm_control_panel.py
+++ homeassistant/components/ialarm/alarm_control_panel.py
@@ -6,7 +6,11 @@ from homeassistant.components.alarm_control_panel.const import (
     SUPPORT_ALARM_ARM_AWAY,
     SUPPORT_ALARM_ARM_HOME,
 )
-from homeassistant.helpers.update_coordinator import CoordinatorEntity
+from homeassistant.core import T
+from homeassistant.helpers.update_coordinator import (
+    CoordinatorEntity,
+    DataUpdateCoordinator,
+)
 
 from .const import DATA_COORDINATOR, DOMAIN
 
@@ -22,34 +26,23 @@ async def async_setup_entry(hass, entry, async_add_entities) -> None:
 class IAlarmPanel(CoordinatorEntity, AlarmControlPanelEntity):
     """Representation of an iAlarm device."""
 
-    @property
-    def device_info(self):
-        """Return device info for this device."""
-        return {
-            "identifiers": {(DOMAIN, self.unique_id)},
-            "name": self.name,
+    _attr_name = "iAlarm"
+    _attr_supported_features = SUPPORT_ALARM_ARM_HOME | SUPPORT_ALARM_ARM_AWAY
+
+    def __init__(self, coordinator: DataUpdateCoordinator[T]) -> None:
+        """Initialize an iAlarm device."""
+        super().__init__(coordinator)
+        self._attr_unique_id = coordinator.mac  # type: ignore
+        self._attr_device_info = {
+            "identifiers": {(DOMAIN, str(self.unique_id))},
+            "name": str(self.name),
             "manufacturer": "Antifurto365 - Meian",
         }
-
-    @property
-    def unique_id(self):
-        """Return a unique id."""
-        return self.coordinator.mac
-
-    @property
-    def name(self):
-        """Return the name."""
-        return "iAlarm"
 
     @property
     def state(self):
         """Return the state of the device."""
         return self.coordinator.state
-
-    @property
-    def supported_features(self) -> int:
-        """Return the list of supported features."""
-        return SUPPORT_ALARM_ARM_HOME | SUPPORT_ALARM_ARM_AWAY
 
     def alarm_disarm(self, code=None):
         """Send disarm command."""
