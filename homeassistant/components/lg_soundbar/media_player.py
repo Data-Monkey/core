3165f0b41f264552e30ab5c4472e6b9b3ba10a7b
Robert Hillis <tkdrob4390@yahoo.com>
2021-07-24 05:00:22 -0400

    Use entity class attributes for lg_soundbar

--- homeassistant/components/lg_soundbar/media_player.py
+++ homeassistant/components/lg_soundbar/media_player.py
@@ -27,29 +27,22 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
 class LGDevice(MediaPlayerEntity):
     """Representation of an LG soundbar device."""
 
+    _attr_should_poll = False
+    _attr_state = STATE_ON
+    _attr_supported_features = SUPPORT_LG
+
     def __init__(self, discovery_info):
         """Initialize the LG speakers."""
         self._host = discovery_info["host"]
         self._port = discovery_info["port"]
-        self._hostname = discovery_info["hostname"]
-
-        self._name = self._hostname.split(".")[0]
+        self._attr_name = discovery_info["hostname"].split(".")[0]
         self._volume = 0
-        self._volume_min = 0
         self._volume_max = 0
         self._function = -1
         self._functions = []
         self._equaliser = -1
         self._equalisers = []
-        self._mute = 0
-        self._rear_volume = 0
-        self._rear_volume_min = 0
-        self._rear_volume_max = 0
-        self._woofer_volume = 0
-        self._woofer_volume_min = 0
-        self._woofer_volume_max = 0
-        self._bass = 0
-        self._treble = 0
+        self._attr_is_volume_muted = True
         self._device = None
 
     async def async_added_to_hass(self):
@@ -67,10 +60,6 @@ class LGDevice(MediaPlayerEntity):
         """Handle responses from the speakers."""
         data = response["data"]
         if response["msg"] == "EQ_VIEW_INFO":
-            if "i_bass" in data:
-                self._bass = data["i_bass"]
-            if "i_treble" in data:
-                self._treble = data["i_treble"]
             if "ai_eq_list" in data:
                 self._equalisers = data["ai_eq_list"]
             if "i_curr_eq" in data:
@@ -79,13 +68,11 @@ class LGDevice(MediaPlayerEntity):
             if "i_vol" in data:
                 self._volume = data["i_vol"]
             if "s_user_name" in data:
-                self._name = data["s_user_name"]
-            if "i_vol_min" in data:
-                self._volume_min = data["i_vol_min"]
+                self._attr_name = data["s_user_name"]
             if "i_vol_max" in data:
                 self._volume_max = data["i_vol_max"]
             if "b_mute" in data:
-                self._mute = data["b_mute"]
+                self._attr_is_volume_muted = data["b_mute"]
             if "i_curr_func" in data:
                 self._function = data["i_curr_func"]
         elif response["msg"] == "FUNC_VIEW_INFO":
@@ -94,22 +81,10 @@ class LGDevice(MediaPlayerEntity):
             if "ai_func_list" in data:
                 self._functions = data["ai_func_list"]
         elif response["msg"] == "SETTING_VIEW_INFO":
-            if "i_rear_min" in data:
-                self._rear_volume_min = data["i_rear_min"]
-            if "i_rear_max" in data:
-                self._rear_volume_max = data["i_rear_max"]
-            if "i_rear_level" in data:
-                self._rear_volume = data["i_rear_level"]
-            if "i_woofer_min" in data:
-                self._woofer_volume_min = data["i_woofer_min"]
-            if "i_woofer_max" in data:
-                self._woofer_volume_max = data["i_woofer_max"]
-            if "i_woofer_level" in data:
-                self._woofer_volume = data["i_woofer_level"]
             if "i_curr_eq" in data:
                 self._equaliser = data["i_curr_eq"]
             if "s_user_name" in data:
-                self._name = data["s_user_name"]
+                self._attr_name = data["s_user_name"]
         self.schedule_update_ha_state()
 
     def update(self):
@@ -121,31 +96,11 @@ class LGDevice(MediaPlayerEntity):
         self._device.get_product_info()
 
     @property
-    def should_poll(self):
-        """No polling needed."""
-        return False
-
-    @property
-    def name(self):
-        """Return the name of the device."""
-        return self._name
-
-    @property
     def volume_level(self):
         """Volume level of the media player (0..1)."""
         if self._volume_max != 0:
             return self._volume / self._volume_max
         return 0
-
-    @property
-    def is_volume_muted(self):
-        """Boolean if volume is currently muted."""
-        return self._mute
-
-    @property
-    def state(self):
-        """Return the state of the device."""
-        return STATE_ON
 
     @property
     def sound_mode(self):
@@ -178,11 +133,6 @@ class LGDevice(MediaPlayerEntity):
             if function < len(temescal.functions):
                 sources.append(temescal.functions[function])
         return sorted(sources)
-
-    @property
-    def supported_features(self):
-        """Flag media player features that are supported."""
-        return SUPPORT_LG
 
     def set_volume_level(self, volume):
         """Set volume level, range 0..1."""
