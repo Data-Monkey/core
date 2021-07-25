ac25890cd54414415b213013aaf668032603acd1
Robert Hillis <tkdrob4390@yahoo.com>
2021-07-25 19:30:58 -0400

    Use entity class attributes for luftdaten

--- homeassistant/components/luftdaten/sensor.py
+++ homeassistant/components/luftdaten/sensor.py
@@ -54,72 +54,20 @@ async def async_setup_entry(hass, entry, async_add_entities):
 class LuftdatenSensor(SensorEntity):
     """Implementation of a Luftdaten sensor."""
 
+    _attr_should_poll = False
+
     def __init__(self, luftdaten, sensor_type, name, icon, unit, device_class, show):
         """Initialize the Luftdaten sensor."""
         self._async_unsub_dispatcher_connect = None
         self.luftdaten = luftdaten
-        self._icon = icon
-        self._name = name
+        self._attr_icon = icon
+        self._attr_name = name
         self._data = None
         self.sensor_type = sensor_type
-        self._unit_of_measurement = unit
+        self._attr_unit_of_measurement = unit
         self._show_on_map = show
-        self._attrs = {}
+        self._attr_extra_state_attributes = {ATTR_ATTRIBUTION: DEFAULT_ATTRIBUTION}
         self._attr_device_class = device_class
-
-    @property
-    def icon(self):
-        """Return the icon."""
-        return self._icon
-
-    @property
-    def state(self):
-        """Return the state of the device."""
-        if self._data is not None:
-            try:
-                return self._data[self.sensor_type]
-            except KeyError:
-                return None
-
-    @property
-    def unit_of_measurement(self):
-        """Return the unit of measurement of this entity, if any."""
-        return self._unit_of_measurement
-
-    @property
-    def should_poll(self):
-        """Disable polling."""
-        return False
-
-    @property
-    def unique_id(self) -> str:
-        """Return a unique, friendly identifier for this entity."""
-        if self._data is not None:
-            try:
-                return f"{self._data['sensor_id']}_{self.sensor_type}"
-            except KeyError:
-                return None
-
-    @property
-    def extra_state_attributes(self):
-        """Return the state attributes."""
-        self._attrs[ATTR_ATTRIBUTION] = DEFAULT_ATTRIBUTION
-
-        if self._data is not None:
-            try:
-                self._attrs[ATTR_SENSOR_ID] = self._data["sensor_id"]
-            except KeyError:
-                return None
-
-            on_map = ATTR_LATITUDE, ATTR_LONGITUDE
-            no_map = "lat", "long"
-            lat_format, lon_format = on_map if self._show_on_map else no_map
-            try:
-                self._attrs[lon_format] = self._data["longitude"]
-                self._attrs[lat_format] = self._data["latitude"]
-                return self._attrs
-            except KeyError:
-                return
 
     async def async_added_to_hass(self):
         """Register callbacks."""
@@ -144,3 +92,30 @@ class LuftdatenSensor(SensorEntity):
             self._data = self.luftdaten.data[DATA_LUFTDATEN]
         except KeyError:
             return
+        if self._data is not None:
+            try:
+                self._attr_state = self._data[self.sensor_type]
+            except KeyError:
+                self._attr_state = None
+            try:
+                self._attr_unique_id = f"{self._data['sensor_id']}_{self.sensor_type}"
+            except KeyError:
+                self._attr_unique_id = None
+            try:
+                self._attr_extra_state_attributes[ATTR_SENSOR_ID] = self._data[
+                    "sensor_id"
+                ]
+                on_map = ATTR_LATITUDE, ATTR_LONGITUDE
+                no_map = "lat", "long"
+                lat_format, lon_format = on_map if self._show_on_map else no_map
+                try:
+                    self._attr_extra_state_attributes[lon_format] = self._data[
+                        "longitude"
+                    ]
+                    self._attr_extra_state_attributes[lat_format] = self._data[
+                        "latitude"
+                    ]
+                except KeyError:
+                    self._attr_extra_state_attributes = None
+            except KeyError:
+                self._attr_extra_state_attributes = None
