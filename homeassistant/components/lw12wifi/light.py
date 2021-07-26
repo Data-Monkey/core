dea9d93a28b328bea1b222f082ddbce29abe9aed
Robert Hillis <tkdrob4390@yahoo.com>
2021-07-25 20:42:49 -0400

    Use entity class attributes for lw12wifi

--- homeassistant/components/lw12wifi/light.py
+++ homeassistant/components/lw12wifi/light.py
@@ -50,6 +50,15 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
 class LW12WiFi(LightEntity):
     """LW-12 WiFi LED Controller."""
 
+    _attr_assumed_state = True
+    _attr_effect_list = [
+        effect.name.replace("_", " ").title() for effect in lw12.LW12_EFFECT
+    ]
+    _attr_should_poll = False
+    _attr_supported_features = (
+        SUPPORT_BRIGHTNESS | SUPPORT_EFFECT | SUPPORT_COLOR | SUPPORT_TRANSITION
+    )
+
     def __init__(self, name, lw12_light):
         """Initialise LW-12 WiFi LED Controller.
 
@@ -57,94 +66,40 @@ class LW12WiFi(LightEntity):
         :param lw12_light: Instance of the LW12 controller.
         """
         self._light = lw12_light
-        self._name = name
-        self._state = None
-        self._effect = None
-        self._rgb_color = [255, 255, 255]
-        self._brightness = 255
-        # Setup feature list
-        self._supported_features = (
-            SUPPORT_BRIGHTNESS | SUPPORT_EFFECT | SUPPORT_COLOR | SUPPORT_TRANSITION
-        )
-
-    @property
-    def name(self):
-        """Return the display name of the controlled light."""
-        return self._name
-
-    @property
-    def brightness(self):
-        """Return the brightness of the light."""
-        return self._brightness
-
-    @property
-    def hs_color(self):
-        """Read back the hue-saturation of the light."""
-        return color_util.color_RGB_to_hs(*self._rgb_color)
-
-    @property
-    def effect(self):
-        """Return current light effect."""
-        if self._effect is None:
-            return None
-        return self._effect.replace("_", " ").title()
-
-    @property
-    def is_on(self):
-        """Return true if light is on."""
-        return self._state
-
-    @property
-    def supported_features(self):
-        """Return a list of supported features."""
-        return self._supported_features
-
-    @property
-    def effect_list(self):
-        """Return a list of available effects.
-
-        Use the Enum element name for display.
-        """
-        return [effect.name.replace("_", " ").title() for effect in lw12.LW12_EFFECT]
-
-    @property
-    def assumed_state(self) -> bool:
-        """Return True if unable to access real state of the entity."""
-        return True
-
-    @property
-    def should_poll(self) -> bool:
-        """Return False to not poll the state of this entity."""
-        return False
+        self._attr_name = name
+        self._attr_hs_color = color_util.color_RGB_to_hs(*[255, 255, 255])
+        self._attr_brightness = 255
 
     def turn_on(self, **kwargs):
         """Instruct the light to turn on."""
         self._light.light_on()
         if ATTR_HS_COLOR in kwargs:
-            self._rgb_color = color_util.color_hs_to_RGB(*kwargs[ATTR_HS_COLOR])
-            self._light.set_color(*self._rgb_color)
-            self._effect = None
+            rgb_color = color_util.color_hs_to_RGB(*kwargs[ATTR_HS_COLOR])
+            self._attr_hs_color = color_util.color_RGB_to_hs(*rgb_color)
+            self._light.set_color(*rgb_color)
+            self._attr_effect = None
         if ATTR_BRIGHTNESS in kwargs:
-            self._brightness = kwargs.get(ATTR_BRIGHTNESS)
-            brightness = int(self._brightness / 255 * 100)
+            self._attr_brightness = kwargs.get(ATTR_BRIGHTNESS)
+            brightness = int(self.brightness / 255 * 100)
             self._light.set_light_option(lw12.LW12_LIGHT.BRIGHTNESS, brightness)
         if ATTR_EFFECT in kwargs:
-            self._effect = kwargs[ATTR_EFFECT].replace(" ", "_").upper()
+            self._attr_effect = kwargs[ATTR_EFFECT].replace(" ", "_").upper()
+            self._attr_effect.replace("_", " ").title()
             # Check if a known and supported effect was selected.
-            if self._effect in [eff.name for eff in lw12.LW12_EFFECT]:
+            if self.effect in [eff.name for eff in lw12.LW12_EFFECT]:
                 # Selected effect is supported and will be applied.
-                self._light.set_effect(lw12.LW12_EFFECT[self._effect])
+                self._light.set_effect(lw12.LW12_EFFECT[self.effect])
             else:
                 # Unknown effect was set, recover by disabling the effect
                 # mode and log an error.
-                _LOGGER.error("Unknown effect selected: %s", self._effect)
-                self._effect = None
+                _LOGGER.error("Unknown effect selected: %s", self.effect)
+                self._attr_effect = None
         if ATTR_TRANSITION in kwargs:
             transition_speed = int(kwargs[ATTR_TRANSITION])
             self._light.set_light_option(lw12.LW12_LIGHT.FLASH, transition_speed)
-        self._state = True
+        self._attr_is_on = True
 
     def turn_off(self, **kwargs):
         """Instruct the light to turn off."""
         self._light.light_off()
-        self._state = False
+        self._attr_is_on = False
